7e49f1e2630c950d842d3c56e7d83e2c354c8d7b
Stephanie Patton <stephaniepatton@Stephanies-MacBook-Air.local>
2019-07-10 17:53:17 -0500

    changed room to outside

--- src/adv.py
+++ src/adv.py
@@ -39,7 +39,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-player = Player('Steph', room['foyer'])
+player = Player('Steph', room['outside'])
 
 # Write a loop that:
 
