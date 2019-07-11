4113712b2163aa9d91d554ee33d70c58acb73392
Stephanie Patton <stephaniepatton@Stephanies-MacBook-Air.local>
2019-07-11 16:34:57 -0500

    connected player to rooms

--- src/player.py
+++ src/player.py
@@ -1,2 +1,16 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from item import Item
+
+class Player:
+    def __init__(self,name, currentRoom, items):
+        self.name = name
+        self.currentRoom = currentRoom,
+        self.items = items
+    def __str__(self):
+        return f'player name: {self.name} current room: {self.currentRoom}'
+    def addItem(self,item):
+        self.items.append(item)
+        item.on_take()
+
+  
